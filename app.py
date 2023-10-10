bbbb607e134ae416a8d319fb8a9f8b521d684e42
gym66 <147191766+gym66@users.noreply.github.com>
2023-10-10 18:26:32 +0000

    saving

--- app.py
+++ app.py
@@ -2,9 +2,11 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+
 @app.route("/")
 def Hello_world():
-    return render_template('home.html')
+  return render_template('home.html')
+
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', debug=True)
+  app.run(host='0.0.0.0', debug=True)
